3c5a87aee4070768e57ebd9c300687ccc38ccba1
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-08-01 12:53:32 +0200

    Add files via upload

---
+++ bash/prepare_images.bash
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+#
+# Prepare image file for minting as NFT.
+# Version 0.0.0.1
+# Copyright © 2024, Dr. Peter Netz
+# Published under the MIT license.
+
+# Set the file type extension.
+FILES=("*.jpg" "*.JPEG" "*.JPG" "*.jpeg" "*.PNG" "*.png")
+
+# Set the global EXIF strings.
+CREATOR="zentrocdot"
+CREATORTOOL="AI Generator Stable Diffusion, AI WebUI AUTOMATIC1111"
+COPYRIGHT="2024, zentrocdot"
+COMMENT="Fantastic Mushroom Collection"
+USERCOMMENT="Selected image for minting as NFT"
+
+# Function create_image.
+create_image () {
+    # Loop over all image files by extension.
+    for ext in "${FILES[@]}"
+    do
+        # Get the file list.
+        list=$(ls ${ext} 2>/dev/null)
+        files=($list)
+        # Loop over all files from the file list.
+        for file in "${files[@]}"
+        do
+            # Check if a filename is a md5 hash.
+            MD5="${file:0:32}"
+            if [[ ! $MD5 =~ ^[a-f0-9]{32}$ ]]
+            then
+                # Get the file extension.
+                extension="${file##*.}"
+                # Create a temporary filename.
+                tmpfn="tmp.${extension}"
+                echo "${file}"
+                # Copy the file to the temporary file.
+                cp "${file}" "${tmpfn}"
+                # Strip all exif data from temporary file.
+                exiftool -all= "${tmpfn}"
+                # Add new exif data to the temporary file.
+                exiftool -comment="${COMMENT}" -usercomment="${USERCOMMENT}" \
+                         -creator="${CREATOR}" -creatortool="${CREATORTOOL}" \
+                         -copyright="${COPYRIGHT}" "${tmpfn}"
+                # Get the md5 hash of the temporary file.
+                HASH=$(md5sum "${tmpfn}" | awk '{print $1}')
+                # Rename the temporary file using the md5 hash as filename.
+                mv "${tmpfn}" "${HASH}"."${extension}"
+                # Remove the not wanted original file.
+                rm "${tmpfn}"_original
+            fi
+        done
+    done
+}
+
+# Call the function.
+create_image
+
+# Exit the script.
+exit 0
