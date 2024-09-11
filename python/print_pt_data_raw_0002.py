f39743ba70b30ec7e7f05bf0ad4bc405f6094131
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-09-11 10:24:15 +0200

    Add files via upload

---
+++ python/print_pt_data_raw_0002.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+''' Read and print data from .pt file.'''
+#
+# Version 0.0.0.2
+#
+# pylint: disable=wrong-import-position
+# pylint: disable=consider-using-sys-exit
+
+# Import the standard Python module
+import sys
+
+# Import module.
+import warnings
+
+# Ignore (torch) future warning.
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Import module.
+import torch
+
+# Set the printout options.
+torch.set_printoptions(threshold=10_000)
+
+# Get the argument from the command-line except the filename.
+PT_FILE = sys.argv[1]
+
+# Main script function.
+def main(filename):
+    '''Main script function.'''
+    # Get data.
+    data=torch.load(filename)
+    # Print data.
+    print(data)
+
+# Execute as module as well as a program.
+if __name__ == "__main__":
+    # Call main function.
+    main(PT_FILE)
