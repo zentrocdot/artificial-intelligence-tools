cd5076b2b045bfb3e31b519dfe290b75aa847e58
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-09-11 17:00:50 +0200

    Update print_pt_data_raw_0001.py

--- python/print_pt_data_raw_0001.py
+++ python/print_pt_data_raw_0001.py
@@ -2,6 +2,8 @@
 ''' Read and print data from .pt file.'''
 #
 # Version 0.0.0.1
+# Copyright © 2024, Dr. Peter Netz
+# Published under the MIT license.
 #
 # pylint: disable=wrong-import-position
 # pylint: disable=consider-using-sys-exit
