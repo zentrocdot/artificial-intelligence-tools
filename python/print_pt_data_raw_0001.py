a1f05f376a4e9735a151c5240f4907db151d7ee0
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-09-11 09:18:08 +0200

    Add files via upload

---
+++ python/print_pt_data_raw_0001.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+''' Read and print data from .pt file.'''
+#
+# Version 0.0.0.1
+#
+# pylint: disable=wrong-import-position
+# pylint: disable=consider-using-sys-exit
+
+# Import the standard Python module
+import sys
+
+# Import module.
+import warnings
+
+# Ignore (torch) future warning.
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Import module.
+import torch
+
+# Get the argument from the command-line except the filename.
+PT_FILE = sys.argv[1]
+
+# Main script function.
+def main(filename):
+    '''Main script function.'''
+    # Get data.
+    data=torch.load(filename)
+    # Print data.
+    print(data)
+
+# Execute as module as well as a program.
+if __name__ == "__main__":
+    # Call main function.
+    main(PT_FILE)
---
+++ python/print_pt_data_v0001.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+''' Structured reading and writing data from .pt file.'''
+#
+# Version 0.0.0.1
+#
+# pylint: disable=wrong-import-position
+# pylint: disable=consider-using-sys-exit
+
+# Import module.
+import warnings
+
+# Ignore (torch) future warning.
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Import the standard Python module
+import sys
+
+# Import the third party Python module.
+import torch
+
+# Get the argument from the command-line except the filename.
+PT_FILE = sys.argv[1]
+
+# Main script function.
+def main(filename):
+    '''Main script function'''
+    # Load pt file into data.
+    data = torch.load(filename)
+    # Loop over key and value in dict.
+    for param in data:
+        # print key and value.
+        print(param, data[param])
+
+# Execute as module as well as a program.
+if __name__ == "__main__":
+    # Call main function.
+    main(PT_FILE)
