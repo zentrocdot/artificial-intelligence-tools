69074204f57db27e018e196104e278f142963792
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-09-12 08:20:17 +0200

    Update README.py

--- python/README.py
+++ python/README.py
@@ -1,1 +1,15 @@
+<hr width="100%" size="2">
 
+<p align="center">I loved the time when you could get also a hamburger :hamburger: for one euro!</p>
+
+<p align="center">
+<a target="_blank" href="https://www.buymeacoffee.com/zentrocdot"><img src="\IMAGES\greeen-button.png" alt="Buy Me A Coffee" height="41" width="174"></a>
+</p>
+<hr width="100%" size="2">
+
+<p align="justify">If you like what I present here, or if it helps you, or if it is useful, you are welcome to donate a small contribution or a cup of coffee. Or as you might say: Every TRON counts! Many thanks in advance! :smiley:</p>
+
+<pre>TQamF8Q3z63sVFWiXgn2pzpWyhkQJhRtW7            (TRON)
+DMh7EXf7XbibFFsqaAetdQQ77Zb5TVCXiX            (DOGE)
+12JsKesep3yuDpmrcXCxXu7EQJkRaAvsc5            (BITCOIN)
+0x31042e2F3AE241093e0387b41C6910B11d94f7ec    (Ethereum)</pre>
