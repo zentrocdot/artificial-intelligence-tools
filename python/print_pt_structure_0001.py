180346c8a7c279ae8f4e658e76a92db151f52133
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-09-12 07:23:06 +0200

    Add files via upload

---
+++ python/print_pt_structure_0001.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+# Version 0.0.0.1
+
+# Import module.
+import zipfile
+
+# Set filename.
+PT_FILE = "embedding_file_name.pt"
+PT_FILE = "hack.pt"
+
+# Open file for reading.
+with zipfile.ZipFile(PT_FILE, mode="r") as archive:
+    # Print file structure.
+    archive.printdir()
+
